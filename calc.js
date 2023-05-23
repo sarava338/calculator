f9f4bf1dcbffc83ac2a62a14ff2802f9cea1f599
sarava338 <sarava338@gmail.com>
2023-05-23 22:11:15 +0530

    code redefined

--- calc.js
+++ calc.js
@@ -3,23 +3,32 @@ var display = document.getElementById('display')
 var newHistory = document.getElementById('new')
 var oldHistory = document.getElementById('old')
 
-buttons.forEach( b => {
+const calculate = () => {
+  try {
+    if (!display.value) alert("Enter Input Value");
+    else {
+      if (!newHistory.innerText) newHistory.innerText = eval(display.value);
+      else {
+        oldHistory.innerText = newHistory.innerText;
+        newHistory.innerText = eval(display.value);
+      }
+      display.value = "";
+    }
+  } catch (err) {
+    display.value = err.message;
+  }
+};
 
-    b.addEventListener('click', () => {
+buttons.forEach((b) => {
+  b.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (b.value == "d") display.value = "";
+    else if (b.value == "c") display.value = display.value.slice(0, -1);
+    else if (b.value == "=") calculate();
+    else display.value += b.value;
+  });
+});
 
-        if(b.value == 'd') display.value = ''
-        else if(b.value == 'c') display.value = display.value.slice(0,-1)
-        else if(b.value == '=') {
-            if(!display.value) alert('Enter Input Value')
-            else{
-                if(!newHistory.innerText) newHistory.innerText = eval(display.value)
-                else {
-                    oldHistory.innerText = newHistory.innerText
-                    newHistory.innerText = eval(display.value)
-                }
-                display.value = ''
-            }
-        }
-        else display.value += b.value
-    })
-})
+display.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") calculate();
+});
